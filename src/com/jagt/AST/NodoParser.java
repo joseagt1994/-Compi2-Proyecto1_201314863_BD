545e3ef535d75c6a02fe0b473ffa0464e308c128
joseagt1994 <josegt10100@gmail.com>
2017-09-08 16:10:21 -0600

    Analizador generado de USQL

--- src/com/jagt/AST/NodoParser.java
+++ src/com/jagt/AST/NodoParser.java
@@ -5,10 +5,37 @@
  */
 package com.jagt.AST;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Jose Antonio
  */
 public class NodoParser {
+    
+    private final String valor,nombre;
+    public final int linea,columna;
+    private final LinkedList<NodoParser> nodosHijos;
+    public int id;
+    
+    public NodoParser(String nombre,String valor,int linea,int columna){
+        this.nombre = nombre;
+        this.valor = valor;
+        this.linea = linea;
+        this.columna = columna;
+        nodosHijos = new LinkedList<NodoParser>();
+    }
+    
+    public LinkedList<NodoParser> hijos(){
+        return nodosHijos;
+    }
+    
+    public String nombre(){
+        return nombre;
+    }
+    
+    public String valor(){
+        return valor;
+    }
     
 }
